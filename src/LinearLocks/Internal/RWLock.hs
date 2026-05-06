fa4395224581aa16df50ed401c1da2cf36fc4275
Diogo Castro <dc@diogocastro.com>
2026-05-06 23:22:45 +0100

    Renamed some types

--- src/LinearLocks/Internal/RWLock.hs
+++ src/LinearLocks/Internal/RWLock.hs
@@ -25,31 +25,26 @@ import System.IO.Resource.Linear (RIO)
 import System.IO.Resource.Linear qualified as RIO
 import System.IO.Resource.Linear.Internal qualified as Internal
 
--- | A deadlock-free mutex.
---
--- This implementation is lazy.
--- This means that if you place an expensive unevaluated thunk inside a t`Mutex`,
--- it will be evaluated by the thread that consumes it, not the thread that produced it.
--- To avoid this, use "LinearLocks.Mutex.Strict" instead.
-data Mutex (lvl :: Nat) a = Mutex
+-- | A deadlock-free lock that allows multiple concurrent readers or a single writer.
+data RWLock (lvl :: Nat) a = RWLock
   { var :: MVar a,
-    -- | The unique ID for this mutex. It's used to ensure t'LinearLocks.MutexSet's don't contain duplicate mutexes, see 'LinearLocks.newMutexSet'.
+    -- | The unique ID for this lock. It's used to ensure t'LinearLocks.MutexSet's don't contain duplicate mutexes, see 'LinearLocks.newMutexSet'.
     id :: MutexId
   }
 
--- | A t`MutexGuard` represents the ownership of a locked mutex.
+-- | A t`RWLockGuard` represents the ownership of a locked RWLock.
 --
 -- It can be used to read/write the mutex while the lock is held.
 --
 -- It must be released with `release`, after which the guard will be consumed and can no longer be used.
-data MutexGuard a = MutexGuard
-  { resource :: RIO.Resource (MutexResource a),
+data RWLockGuard a = RWLockGuard
+  { resource :: RIO.Resource (RWLockResource a),
     -- | The latest value set by the user.
     -- This will be comitted to the MVar when the guard is released.
     newValue :: Ur a
   }
 
-data MutexResource a = MutexResource
+data RWLockResource a = RWLockResource
   { -- | The value that was read from the `MVar` when it was acquired.
     --
     -- If an exception occurs before the mutex guard is manually released, this value will be put back into the `MVar`.
@@ -57,50 +52,50 @@ data MutexResource a = MutexResource
     var :: MVar a
   }
 
-instance Lockable (Mutex lvl a) where
-  type Guard (Mutex lvl a) = MutexGuard a
-  type Level (Mutex lvl a) = lvl
+instance Lockable (RWLock lvl a) where
+  type Guard (RWLock lvl a) = RWLockGuard a
+  type Level (RWLock lvl a) = lvl
 
   getId m = m.id
 
-  unsafeLock :: forall lvl a. Mutex lvl a -> RIO (MutexGuard a)
+  unsafeLock :: forall lvl a. RWLock lvl a -> RIO (RWLockGuard a)
   unsafeLock m = L.do
     -- Note: we have to match on `UnsafeResource` so we can extract the `guard.initialValue`
     Internal.UnsafeResource key guard <- RIO.unsafeAcquire acq rel
     L.pure
-      MutexGuard
+      RWLockGuard
         { resource = Internal.UnsafeResource key guard,
           newValue = Ur guard.initialValue
         }
     where
-      acq :: L.IO (Ur (MutexResource a))
+      acq :: L.IO (Ur (RWLockResource a))
       acq = L.do
         Ur a <- L.fromSystemIOU L.$ MVar.takeMVar m.var
-        L.pure (Ur (MutexResource {initialValue = a, var = m.var}))
+        L.pure (Ur (RWLockResource {initialValue = a, var = m.var}))
 
       -- The action to run if an exception is thrown before the guard is manually released with `release`.
-      rel :: MutexResource a -> L.IO ()
-      rel (MutexResource initialValue var) =
+      rel :: RWLockResource a -> L.IO ()
+      rel (RWLockResource initialValue var) =
         L.void L.$ L.fromSystemIO L.$ MVar.putMVar var initialValue
 
-instance Releasable (MutexGuard a) where
+instance Releasable (RWLockGuard a) where
   doRelease = release
 
-read :: MutexGuard a %1 -> RIO (Ur a, MutexGuard a)
-read (MutexGuard resource (Ur newValue)) =
-  L.pure (Ur newValue, MutexGuard {resource, newValue = Ur newValue})
+read :: RWLockGuard a %1 -> RIO (Ur a, RWLockGuard a)
+read (RWLockGuard resource (Ur newValue)) =
+  L.pure (Ur newValue, RWLockGuard {resource, newValue = Ur newValue})
 
 -- | Writes a new value to the mutex, which will be committed when the guard is released.
 --
 -- If an exception is thrown after `write` but before `release`,
 -- the mutex will be rolled back to its original state.
-write :: MutexGuard a %1 -> a -> RIO (MutexGuard a)
-write (MutexGuard resource (Ur _)) newValue =
-  L.pure (MutexGuard {resource, newValue = Ur newValue})
+write :: RWLockGuard a %1 -> a -> RIO (RWLockGuard a)
+write (RWLockGuard resource (Ur _)) newValue =
+  L.pure (RWLockGuard {resource, newValue = Ur newValue})
 
 -- | Releases a mutex and commits the latest value set by `write`.
-release :: MutexGuard a %1 -> RIO ()
-release (MutexGuard ((Internal.UnsafeResource key mr)) (Ur newValue)) = L.do
+release :: RWLockGuard a %1 -> RIO ()
+release (RWLockGuard ((Internal.UnsafeResource key mr)) (Ur newValue)) = L.do
   -- Note: the resource was initially registered with a release action that puts the original value back into the MVar.
   -- That release action should be run if an exception is thrown before `release` is called,
   -- which ensures the MVar will "rollback" to its original state.
@@ -117,12 +112,12 @@ release (MutexGuard ((Internal.UnsafeResource key mr)) (Ur newValue)) = L.do
 --
 -- It does not have to be unique, multiple mutexes can have the same level.
 -- Mutexes with the same level can be added to a t`LinearLocks.MutexSet` and acquired with 'LinearLocks.lockMany'.
-new :: forall a. forall (lvl :: Nat) -> a -> IO (Mutex lvl a)
+new :: forall a. forall (lvl :: Nat) -> a -> IO (RWLock lvl a)
 new _lvl a = do
   var <- MVar.newMVar a
   newId <- Atomic.incrCounter 1 mutexIdCounter
   pure
-    Mutex
+    RWLock
       { var = var,
         id = MutexId newId
       }
