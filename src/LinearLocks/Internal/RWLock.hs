50a3004b1f626244e7c0329074b04ba07e1e7d27
Diogo Castro <dc@diogocastro.com>
2026-05-07 15:30:37 +0100

    Implemented "write mode" for RWLock

--- src/LinearLocks/Internal/RWLock.hs
+++ src/LinearLocks/Internal/RWLock.hs
@@ -9,14 +9,11 @@
 
 module LinearLocks.Internal.RWLock where
 
-import Control.Concurrent (MVar)
-import Control.Concurrent qualified as MVar
 import Control.Concurrent.ReadWriteLock qualified as Conc
 import Control.Functor.Linear qualified as L
 import Control.Monad.IO.Class.Linear qualified as L
 import Data.IORef (IORef)
 import Data.IORef qualified as IORef
-import Data.IntMap.Strict qualified as IntMap
 import Data.Kind (Type)
 import GHC.TypeLits (Nat)
 import LinearLocks.Internal
@@ -25,7 +22,6 @@ import Prelude.Linear qualified as L hiding (IO)
 import System.IO.Linear qualified as L
 import System.IO.Resource.Linear (RIO)
 import System.IO.Resource.Linear qualified as RIO
-import System.IO.Resource.Linear.Internal qualified as Internal
 
 -- | A deadlock-free lock that allows multiple concurrent readers or a single writer.
 data RWLock (lvl :: Nat) a = RWLock
@@ -59,14 +55,14 @@ class Readable guard where
 
 -- | A t`ReadGuard` represents the ownership of a RWLock in read mode.
 --
--- It must be released with `release`, after which the guard will be consumed and can no longer be used.
+-- It must be released with `releaseRead`, after which the guard will be consumed and can no longer be used.
 data ReadGuard a = ReadGuard
-  { resource :: RIO.Resource ReadResource,
+  { resource :: RIO.Resource Resource,
     -- | The value that was read when the lock was acquired.
     readValue :: Ur a
   }
 
-newtype ReadResource = ReadResource
+newtype Resource = Resource
   { lock :: Conc.RWLock
   }
 
@@ -89,14 +85,19 @@ instance Lockable (AsRead lvl a) where
           readValue = Ur readValue
         }
     where
-      acq :: L.IO (Ur ReadResource)
+      acq :: L.IO (Ur Resource)
       acq = L.do
         L.fromSystemIO L.$ Conc.acquireRead m.lock
-        L.pure (Ur (ReadResource {lock = m.lock}))
+        L.pure (Ur (Resource {lock = m.lock}))
 
-      rel :: ReadResource -> L.IO ()
-      rel (ReadResource lock) =
+      rel :: Resource -> L.IO ()
+      rel (Resource lock) =
         L.fromSystemIO L.$ Conc.releaseRead lock
+
+-- | Releases the lock.
+releaseRead :: ReadGuard a %1 -> RIO ()
+releaseRead (ReadGuard resource (Ur _readValue)) =
+  RIO.release resource
 
 instance Releasable (ReadGuard a) where
   doRelease = releaseRead
@@ -108,7 +109,70 @@ instance Readable (ReadGuard a) where
   read (ReadGuard resource (Ur readValue)) =
     L.pure (Ur readValue, ReadGuard {resource, readValue = Ur readValue})
 
--- | Releases the lock.
-releaseRead :: ReadGuard a %1 -> RIO ()
-releaseRead (ReadGuard resource (Ur _readValue)) =
+----------------------------------------------------------------------------
+-- Write mode
+----------------------------------------------------------------------------
+
+-- | A t`WriteGuard` represents the ownership of a RWLock in write mode.
+--
+-- It must be released with `releaseWrite`, after which the guard will be consumed and can no longer be used.
+data WriteGuard a = WriteGuard
+  { resource :: RIO.Resource Resource,
+    -- | The latest value set by the user.
+    -- This will be comitted when the guard is released.
+    newValue :: Ur a,
+    var :: Ur (IORef a)
+  }
+
+newtype AsWrite lvl a = AsWrite (RWLock lvl a)
+
+instance Lockable (AsWrite lvl a) where
+  type Guard (AsWrite lvl a) = WriteGuard a
+  type Level (AsWrite lvl a) = lvl
+
+  getId (AsWrite m) = m.id
+
+  unsafeLock :: forall lvl a. AsWrite lvl a -> RIO (WriteGuard a)
+  unsafeLock (AsWrite m) = L.do
+    -- Acquire the rwlock in "read mode" and *then* read the `IORef`.
+    resource <- RIO.unsafeAcquire acq rel
+    Ur initialValue <- L.liftSystemIOU (IORef.readIORef m.var)
+    L.pure
+      WriteGuard
+        { resource = resource,
+          newValue = Ur initialValue,
+          var = Ur m.var
+        }
+    where
+      acq :: L.IO (Ur Resource)
+      acq = L.do
+        L.fromSystemIO L.$ Conc.acquireRead m.lock
+        L.pure (Ur (Resource {lock = m.lock}))
+
+      rel :: Resource -> L.IO ()
+      rel (Resource lock) =
+        L.fromSystemIO L.$ Conc.releaseRead lock
+
+-- | Releases the lock and commits the latest value set by `write`.
+releaseWrite :: WriteGuard a %1 -> RIO ()
+releaseWrite (WriteGuard resource (Ur newValue) (Ur var)) = L.do
+  L.liftSystemIO $ IORef.writeIORef var newValue
   RIO.release resource
+
+instance Releasable (WriteGuard a) where
+  doRelease = releaseWrite
+
+instance Readable (WriteGuard a) where
+  type Elem (WriteGuard a) = a
+
+  read :: WriteGuard a %1 -> RIO (Ur a, WriteGuard a)
+  read (WriteGuard resource (Ur newValue) var) =
+    L.pure (Ur newValue, WriteGuard {resource, newValue = Ur newValue, var})
+
+-- | Writes a new value to the 'RWLock', which will be committed when the guard is released.
+--
+-- If an exception is thrown after `write` but before `releaseWrite`,
+-- the 'RWLock' will be rolled back to its original state.
+write :: WriteGuard a %1 -> a -> RIO (WriteGuard a)
+write (WriteGuard resource (Ur _) var) newValue =
+  L.pure (WriteGuard {resource, newValue = Ur newValue, var})
